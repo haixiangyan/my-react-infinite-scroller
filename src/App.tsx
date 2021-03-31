689356db1429dbcec714dcdca92a4cedc6d7fff8
Haixiang <haixiang6123@gmail.com>
2021-03-31 23:08:00 +0800

    feat: 添加滚动监听器

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import InfiniteScroll from './lib/InfiniteScroll'
+
+const items = [
+  'hello',
+  'world',
+  'xxx',
+  'aaaa',
+  'bbbb',
+  'cccc',
+  'ddd',
+  'eee',
+  'fff',
+  'www',
+  'qqq',
+  'zzz',
+  'ppp'
+]
 
 const App = () => {
-  return <div>Hello</div>
+  return (
+    <div>
+      <div style={{height: 100, overflow: 'auto', border: '1px solid red'}}>
+        <InfiniteScroll
+          throttle={300}
+          loadMore={() => console.log('load more')}
+          loader={<div className="loader" key={0}>Loading ...</div>}
+        >
+          {items.map(item => <div key={item}>{item}</div>)}
+        </InfiniteScroll>
+      </div>
+    </div>
+  )
 }
 
 export default App
