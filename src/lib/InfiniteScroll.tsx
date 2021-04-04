32fe6b460b593332c29c63c1f71cf9c4bf41e59a
HaixiangYan <haixiang6123@gmail.com>
2021-04-04 13:18:25 +0800

    feat: 添加事件的 useCapture 和 passive

--- src/lib/InfiniteScroll.tsx
+++ src/lib/InfiniteScroll.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import {Component, ReactNode} from 'react'
 
+interface EventListenerOptions {
+  useCapture: boolean
+  passive: boolean
+}
+
 interface Props {
   loadMore: (pageLoaded: number) => void // 加载更多的回调
   loader: ReactNode // 显示 Loading 的元素
@@ -10,12 +15,14 @@ interface Props {
   pageStart?: number // 页面初始页
   getScrollParent?: () => HTMLElement // 获取 parentElement 的回调
   useWindow?: boolean // 是否以 window 作为 scrollEl
+  useCapture?: boolean // 是否注册为捕获事件
 }
 
 class InfiniteScroll extends Component<Props, any> {
   private scrollComponent: HTMLDivElement | null = null // 当前滚动的组件
   private loadingMore = false // 是否正在加载更多
   private pageLoaded = 0 // 当前加载页数
+  private eventOptions = {} // 注册事件的选项
   // isReverse 后专用参数
   private beforeScrollTop = 0 // 上次滚动时 parentNode 的 scrollTop
   private beforeScrollHeight = 0 // 上次滚动时 parentNode 的 scrollHeight
@@ -27,13 +34,45 @@ class InfiniteScroll extends Component<Props, any> {
     hasMore: true,
     pageStart: 0,
     getScrollParent: null,
-    useWindow: true
+    useWindow: true,
+    useCapture: false
   }
 
   constructor(props: Props) {
     super(props);
+    this.getEventListenerOptions = this.getEventListenerOptions.bind(this)
     this.scrollListener = this.scrollListener.bind(this)
     this.mousewheelListener = this.mousewheelListener.bind(this)
+  }
+
+  isPassiveSupported() {
+    let passive = false
+
+    const testOptions = {
+      get passive() {
+        passive = true
+        return true
+      }
+    }
+
+    try {
+      const testListener = () => {}
+      document.addEventListener('test', testListener, testOptions)
+      // @ts-ignore 仅用于测试是否可以使用 passive listener
+      document.removeEventListener('test', testListener, testOptions)
+    } catch (e) {}
+
+    return passive
+  }
+
+  getEventListenerOptions() {
+    const options: EventListenerOptions = {useCapture: this.props.useCapture || false, passive: false}
+
+    if (this.isPassiveSupported()) {
+      options.passive = true
+    }
+
+    return options
   }
 
   scrollListener() {
@@ -71,7 +110,7 @@ class InfiniteScroll extends Component<Props, any> {
   mousewheelListener(e: Event) {
     // 详见: https://stackoverflow.com/questions/47524205/random-high-content-download-time-in-chrome/47684257#47684257
     // @ts-ignore mousewheel 事件里存在 deltaY
-    if (e.deltaY === 1) {
+    if (e.deltaY === 1 && !this.isPassiveSupported()) {
       e.preventDefault()
     }
   }
@@ -105,9 +144,9 @@ class InfiniteScroll extends Component<Props, any> {
 
     const scrollEl = this.props.useWindow ? window : parentElement
 
-    scrollEl.addEventListener('scroll', this.scrollListener)
-    scrollEl.addEventListener('resize', this.scrollListener)
-    scrollEl.addEventListener('mousewheel', this.mousewheelListener)
+    scrollEl.addEventListener('scroll', this.scrollListener, this.eventOptions)
+    scrollEl.addEventListener('resize', this.scrollListener, this.eventOptions)
+    scrollEl.addEventListener('mousewheel', this.mousewheelListener, this.eventOptions)
   }
 
   detachMousewheelListener() {
@@ -115,7 +154,7 @@ class InfiniteScroll extends Component<Props, any> {
 
     if (!scrollEl) return
 
-    scrollEl.removeEventListener('mousewheel', this.mousewheelListener)
+    scrollEl.removeEventListener('mousewheel', this.mousewheelListener, this.eventOptions)
   }
 
   detachListeners() {
@@ -123,13 +162,14 @@ class InfiniteScroll extends Component<Props, any> {
 
     if (!scrollEl) return
 
-    scrollEl.removeEventListener('scroll', this.scrollListener)
-    scrollEl.removeEventListener('resize', this.scrollListener)
+    scrollEl.removeEventListener('scroll', this.scrollListener, this.eventOptions)
+    scrollEl.removeEventListener('resize', this.scrollListener, this.eventOptions)
   }
 
   componentDidMount() {
-    this.attachListeners()
     this.pageLoaded = this.props.pageStart || 0
+    this.eventOptions = this.getEventListenerOptions()
+    this.attachListeners()
   }
 
   componentDidUpdate() {
