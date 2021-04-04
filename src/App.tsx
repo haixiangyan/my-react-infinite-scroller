a9dcf7e64818ab4f1eb18687962d3d74ae8db143
HaixiangYan <haixiang6123@gmail.com>
2021-04-04 14:27:30 +0800

    feat: 去掉 window reverse scroller

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,18 @@ import React, {useState} from 'react'
 import DivScroller from './examples/DivScroller'
 import WindowScroller from './examples/WindowScroller'
 import DivReverseScroller from "./examples/DivReverseScroller";
-import WindowReverseScroller from "./examples/WindowReverseScroller";
 
-type Pane = '1' | '2' | '3' | '4'
+type Pane = '1' | '2' | '3'
 
 const App = () => {
   const [pane, setPane] = useState<Pane>('1');
 
   return (
     <div>
-      {pane === '4' && <WindowReverseScroller/>}
-
       <div>
         <button onClick={() => setPane('1')}>Div 向下无限滚动</button>
         <button onClick={() => setPane('2')}>Window 向下无限滚动</button>
         <button onClick={() => setPane('3')}>Div 向上无限滚动</button>
-        <button onClick={() => setPane('4')}>Window 向上无限滚动</button>
       </div>
 
       <div>
